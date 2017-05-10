07bd09c2b366d13e867118349cda3505c1be7776
Alisa Filatova <alicefill@bk.ru>
2017-05-10 21:45:10 +0300

    исправила баг в меню

--- js/main-nav.js
+++ js/main-nav.js
@@ -1,16 +1,16 @@
+
 var nav = document.querySelector('.main-nav');
 var open = document.querySelector('.menu-open');
 var close = document.querySelector('.close-btn');
 
-
 open.addEventListener('click', function(event) {
      event.preventDefault();
      nav.classList.add('main-nav_open');
-     open.classList.remove('menu-open');
+     open.classList.add('menu-open_hidden');
 });
 
 close.addEventListener('click', function(event) {
     event.preventDefault();
     nav.classList.remove('main-nav_open');
-    open.classList.add('menu-open');
+    open.classList.remove('menu-open_hidden');
 });

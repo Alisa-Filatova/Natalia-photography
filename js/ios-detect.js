3c9618333e3f67d157d279da7df9c8a44a6316d0
Alisa Filatova <alicefill@bk.ru>
2017-06-03 15:14:40 +0300

    внесена правка в ios-detect.js

--- js/ios-detect.js
+++ js/ios-detect.js
@@ -2,8 +2,6 @@
  * Created by Alisa on 03.06.17.
  */
 
-var userAgent = window.navigator.userAgent;
-
-if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
+if (window.navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
     document.documentElement.classList.add('ios-safari');
 }

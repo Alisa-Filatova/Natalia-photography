3c2a4b7773c38424e547170c3cf04502cbe09d68
Alisa Filatova <alicefill@bk.ru>
2017-06-04 00:53:30 +0300

    сделана отправка сообщения через форму

---
+++ js/contact.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Alisa on 04.06.17.
+ */
+
+var $success = $('.alert_success');
+var $error = $('.alert_error');
+
+$('.contact__form form').on('submit', function(event) {
+    event.preventDefault();
+
+    var $this = $(this);
+    var request = $.post('/enroll', $this.serialize());
+
+    request.done(function() {
+        $success.addClass('alert_show');
+    });
+
+    request.fail(function() {
+        $error.addClass('alert_show');
+    });
+});
+
+$('.alert__btn').on('click', function() {
+    $(this).closest('.alert').removeClass('alert_show');
+});
